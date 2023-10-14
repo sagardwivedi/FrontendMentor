c2845a2b24fda249baf2a64ba158a0f3c4938edb
sagardwivedi <sagar.dw20@gmail.com>
2023-10-14 12:45:13 +0530

    Change in CCS and structure

--- huddle_landing_intro/app/page.tsx
+++ huddle_landing_intro/app/page.tsx
@@ -18,52 +18,39 @@ const Icon = ({ icon: Icon }: IconProps) => {
 
 const Home = () => {
   return (
-    <div className="flex flex-col gap-y-10 max-lg:m-10 lg:mx-14 lg:mt-7">
-      <Image
-        className="max-lg:hidden"
-        src={"/logo.svg"}
-        width={300}
-        height={100}
-        alt=""
-        priority
-      />
-      <Image
-        className="lg:hidden"
-        src={"/logo.svg"}
-        width={150}
-        height={100}
-        alt=""
-        priority
-      />
-      <section className="flex flex-col max-lg:gap-y-12 lg:flex-row lg:gap-x-10">
-        <div className="lg:w-[50%]">
-          <Image
-            src={"/illustration-mockups.svg"}
-            alt=""
-            width={100}
-            height={100}
-            className="h-auto w-full"
-          />
+    <div className="main-image max-w-[1440px]  mx-auto min-h-screen">
+      <div className="flex flex-col max-md:py-8 md:p-16 w-[90%] mx-auto">
+        <div className="logo relative"></div>
+        <div className="flex flex-col md:flex-row max-md:gap-y-10 mt-16 md:mt-20 md:gap-x-10 md:items-start">
+          <div className="md:w-[70%] md:h-[35rem] relative h-[15rem] w-full">
+            <Image
+              src={"/illustration-mockups.svg"}
+              alt=""
+              fill
+              className="bg-contain"
+              priority
+            />
+          </div>
+          <div className="text-white md:w-[30%] max-md:text-center pt-5 md:pt-10 md:pl-5">
+            <h1 className="text-2xl md:text-4xl font-popins font-semibold tracking-wide md:leading-[3rem]">
+              Build The Community Your Fans Will Love
+            </h1>
+            <p className="font-open_sans mt-5 md:mt-7">
+              Huddle re-imagines the way we build communities. You have a voice,
+              but so does your audience. Create connections with your users as
+              you engage in genuine discussion.
+            </p>
+            <button className="mt-5 bg-white rounded-full text-violet shadow-2xl shadow-violet py-4 w-[50%] hover:bg-soft_magneta hover:text-white transition">
+              Register
+            </button>
+          </div>
         </div>
-        <div className="h-min space-y-4 text-white max-lg:text-center lg:w-[38%]">
-          <h1 className="font-popins text-2xl font-semibold leading-10 tracking-wide lg:text-4xl">
-            Build The Community Your Fans Will Love
-          </h1>
-          <p className="font-open_sans">
-            Huddle re-imagines the way we build communities. You have a voice,
-            but so does your audience. Create connections with your users as you
-            engage in genuine discussion.
-          </p>
-          <button className="h-12 w-full rounded-full bg-white text-violet shadow-lg shadow-violet transition-colors hover:bg-soft_magneta hover:text-white lg:w-[30%]">
-            Register
-          </button>
-        </div>
-      </section>
-      <section className="flex flex-row justify-center gap-x-5 lg:justify-end">
-        <Icon icon={BiLogoFacebook} />
-        <Icon icon={FaXTwitter} />
-        <Icon icon={FaInstagram} />
-      </section>
+        <section className="flex flex-row justify-center mt-8 gap-x-5 lg:justify-end">
+          <Icon icon={BiLogoFacebook} />
+          <Icon icon={FaXTwitter} />
+          <Icon icon={FaInstagram} />
+        </section>
+      </div>
     </div>
   );
 };
